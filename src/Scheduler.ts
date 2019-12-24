29a645aeec1c94d86ec92f451044eff9477abaf9
Doma <leishenghao@126.com>
2019-12-24 16:02:13 +0800

    修复定时任务提前退出的问题

--- src/Scheduler.ts
+++ src/Scheduler.ts
@@ -1,37 +1,45 @@
 import { autoInjectable, inject } from "tsyringe";
-import { Scheduler as SchedulerContract, Container, RequestHandler, Request } from "../types";
+import {
+  Scheduler as SchedulerContract,
+  Container,
+  RequestHandler,
+  Request
+} from "../types";
 import ScheduledTask from "./ScheduledTask";
 
 @autoInjectable()
 export default class Scheduler implements SchedulerContract {
+  protected $tasks: ScheduledTask[] = [];
 
-    protected $tasks: ScheduledTask[] = [];
+  constructor(@inject("container") protected $container: Container) {}
 
-    constructor(
-        @inject('container') protected $container: Container
-    ) {
+  register(cron: string, handler: RequestHandler) {
+    this.$tasks.push(new ScheduledTask(cron, handler));
+
+    return this;
+  }
+
+  async dispatch(request: Request) {
+    console.info("正在准备定时任务…");
+
+    const context = this.$container.resolve("context");
+    const dueTasks = this.$tasks.filter(task => task.isDue());
+
+    if (!dueTasks.length) {
+      console.info("没有需要执行的定时任务。");
     }
 
-    register(cron: string, handler: RequestHandler) {
-        this.$tasks.push(new ScheduledTask(cron, handler));
-
-        return this;
+    let successCount = 0;
+    for (const task of dueTasks) {
+      console.info(`执行定时任务：${task.getDisplayName()}`);
+      try {
+        await task.run(request, context);
+        successCount++;
+      } catch (e) {
+        console.error(e);
+      }
     }
 
-    async dispatch(request: Request) {
-
-        console.info('正在准备定时任务…');
-
-        const context = this.$container.resolve('context');
-        const dueTasks = this.$tasks.filter(task => task.isDue());
-
-        if (!dueTasks.length) {
-            console.info('没有需要执行的定时任务。');
-        }
-
-        dueTasks.forEach(task => {
-            console.info(`执行定时任务：${task.getDisplayName()}`);
-            task.run(request, context);
-        });
-    }
+    return successCount;
+  }
 }
